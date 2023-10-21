c73167949a04e9adbb56d8da68f5b51d5ea0d8a2
Abdurahmon <MrAbuDevJohnson@gmail.com>
2023-10-21 14:06:22 +0300

    Knockout

---
+++ data/config.py
@@ -0,0 +1,9 @@
+TOKEN = '5319874718:AAF31RRmwBKvmQ2L-Mf1ycMKt55ci4saHeo'  # @proweb_practical_bot
+
+DB_NAME = 'sr_sb_17_30'
+DB_HOST = 'localhost'
+DB_PASSWORD = '123456'
+DB_USER = 'postgres'
+
+
+
